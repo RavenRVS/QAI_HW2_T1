366971d102b59d20f51c7571907874d305f3dd02
Vitaly Remnev <remnevvs@mail.ru>
2022-08-18 18:24:16 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 2000;
+        int allBonus;
+        int currentBonus = ticketCost / 20;
+        allBonus = +currentBonus;
+        System.out.println("Количество начисленных бонусов: " + currentBonus);
+        System.out.println("Общее количество бонусов: " + allBonus);
+
+    }
+}
